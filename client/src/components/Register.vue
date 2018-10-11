b2013c42f127f507e0fca4505eed2371caa79747
Rqajendra <rajendraprasadmalla90@gmail.com>
2018-10-11 18:50:58 +0530

    seting up registration page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <h1>Registration</h1>
+    <input type="email" name="email" v-model="email" placeholder="email" />
+    <input type="password" name="password" v-model="password" placeholder="password" />
+    <button @click="register"> Register</button>
+  </div>
+</template>
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  // name: 'Register',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      alert(response.data.message)
+    }
+  }
+}
+</script>
--- server/src/app.js
+++ server/src/app.js
@@ -4,16 +4,16 @@ const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
-app.use(morgan('combind'))
+app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/req', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
-    message: 'hello world i am coming'
+    message: `hello world i ${req.body.email} coming`
   })
 })
 
-app.listen(process.env.PORT || 8081)
+app.listen(process.env.PORT || 1234)
 
 // Run app, then load http://localhost:port in a browser to see the output.
